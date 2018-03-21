8cba69bce283ff1a4d79d714789cf2a95431531b
Lutando Ngqakaza <lutando@ngqakaza.com>
2018-03-21 14:20:21 +0100

    Simple

---
+++ src/Akkatecture/Akka/ShardIdentityExtractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Akkatecture.Core;
+
+namespace Akkatecture.Akka
+{
+    public class ShardIdentityExtractor
+    {
+        public static Tuple<string, object> IdentityExtrator(object message)
+        {
+            if (message is IIdentity command)
+                return new Tuple<string, object>(command.Value, message);
+
+            throw new ArgumentNullException(nameof(message));
+        }
+    }
+    
+}
