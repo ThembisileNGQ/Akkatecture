85837f8454b156a574ea6bddf1d651093c07006e
Lutando Ngqakaza <lutando@ngqakaza.com>
2018-04-03 17:47:50 +0200

    added files

---
+++ test/Akkatecture.Tests/UnitTests/Commands/DistinctCommandTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Akkatecture.Tests.UnitTests.Commands
+{
+    public class DistinctCommandTests
+    {
+        /*[Theory]
+        [InlineData("test-4b1e7b48-18f1-4215-91d9-903cffdab3d8", 1, "command-40867fe4-94da-59e4-9bb5-e532f6565751")]
+        [InlineData("test-4b1e7b48-18f1-4215-91d9-903cffdab3d8", 2, "command-df0e238e-676b-500e-b962-76fcef97768a")]
+        [InlineData("test-6a2a04bd-bbc8-44ac-80ac-b0ca56897bc0", 2, "command-0455a861-bc9e-56c5-b7b9-5c14671db8b2")]
+        public void Arguments(string aggregateId, int magicNumber, string expectedSouceId)
+        {
+            var testId = TestId.With(aggregateId);
+            var command = new MyDistinctCommand(testId, magicNumber);
+
+            var sourceId = command.SourceId;
+            
+            sourceId.Value.Should().Be(expectedSouceId);
+        }
+
+        public class MyDistinctCommand : DistinctCommand<TestAggregate, TestId>
+        {
+            public int MagicNumber { get; }
+
+            public MyDistinctCommand(
+                TestId aggregateId,
+                int magicNumber) : base(aggregateId)
+            {
+                MagicNumber = magicNumber;
+            }
+
+            protected override IEnumerable<byte[]> GetSourceIdComponents()
+            {
+                yield return BitConverter.GetBytes(MagicNumber);
+                yield return AggregateId.GetBytes();
+            }
+        }*/
+    }
+}
---
+++ test/Akkatecture.Tests/UnitTests/Core/CircularBufferTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Akkatecture.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace Akkatecture.Tests.UnitTests.Core
+{
+    public class CircularBufferTests
+    {
+        [Theory]
+        [InlineData(1)] // Below capacity
+        [InlineData(1, 2)] // At capacity
+        [InlineData(1, 2, 3)] // Once above capacity
+        [InlineData(1, 2, 3, 4)] // Loop twice over capacity
+        [InlineData(1, 2, 3, 4, 5)] // One more than of capacity
+        public void CircularBuffer_Putting_ShouldContainCapacity(params int[] numbers)
+        {
+            const int capacity = 2;
+            var sut = new CircularBuffer<int>(capacity);
+
+            foreach (var number in numbers)
+            {
+                sut.Put(number);
+            }
+            
+            var shouldContain = numbers.Reverse().Take(capacity).ToList();
+            sut.Should().Contain(shouldContain);
+        }
+    }
+}
