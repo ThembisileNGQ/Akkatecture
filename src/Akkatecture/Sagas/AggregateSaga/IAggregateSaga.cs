569cf9e507b999d0531107a9326296cd830708b5
Lutando Ngqakaza <lutando@ngqakaza.com>
2018-04-04 09:59:34 +0200

    some saga agg primitives

---
+++ src/Akkatecture/Sagas/AggregateSaga/IAggregateSaga.cs
@@ -0,0 +1,11 @@
+﻿using Akkatecture.Aggregates;
+using Akkatecture.Core;
+
+namespace Akkatecture.Sagas.AggregateSaga
+{
+    public interface IAggregateSaga<out TIdentity, TLocator> : ISaga<TLocator>, IAggregateRoot<TIdentity>
+        where TIdentity : IIdentity
+        where TLocator : ISagaLocator
+    {
+    }
+}
