1961590386f97ce358e98d103ae1edcdbccfdc15
Unknown <lutando.ngqakaza@recordunion.com>
2019-06-18 13:13:02 +0200

    wip

---
+++ test/Akkatecture.MultiNodeTestRunner/Discovery.cs
@@ -0,0 +1,149 @@
+//-----------------------------------------------------------------------
+// <copyright file="Discovery.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2018 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using Akka.MultiNodeTestRunner.Shared;
+using Akka.Remote.TestKit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Akka.MultiNodeTestRunner
+{
+#if CORECLR
+    public class Discovery : IMessageSink, IDisposable
+#else
+    public class Discovery : MarshalByRefObject, IMessageSink, IDisposable
+#endif
+    {
+        public Dictionary<string, List<NodeTest>> Tests { get; set; }
+        public List<ErrorMessage> Errors { get; } = new List<ErrorMessage>();
+        public bool WasSuccessful => Errors.Count == 0;
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Discovery"/> class.
+        /// </summary>
+        public Discovery()
+        {
+            Tests = new Dictionary<string, List<NodeTest>>();
+            Finished = new ManualResetEvent(false);
+        }
+
+        public ManualResetEvent Finished { get; private set; }
+        public IMessageSink NextSink { get; private set; }
+
+        public virtual bool OnMessage(IMessageSinkMessage message)
+        {
+            switch (message)
+            {
+                case ITestCaseDiscoveryMessage testCaseDiscoveryMessage:
+                    var testClass = testCaseDiscoveryMessage.TestClass.Class;
+                    if (testClass.IsAbstract) return true;
+#if CORECLR
+                    var specType = testCaseDiscoveryMessage.TestAssembly.Assembly.GetType(testClass.Name).ToRuntimeType();
+#else
+                    var testAssembly = Assembly.LoadFrom(testCaseDiscoveryMessage.TestAssembly.Assembly.AssemblyPath);
+                    var specType = testAssembly.GetType(testClass.Name);
+#endif
+                    var roles = RoleNames(specType);
+
+                    var details = roles.Select((r, i) => new NodeTest
+                    {
+                        Node = i + 1,
+                        Role = r.Name,
+                        TestName = testClass.Name,
+                        TypeName = testClass.Name,
+                        MethodName = testCaseDiscoveryMessage.TestCase.TestMethod.Method.Name,
+                        SkipReason = testCaseDiscoveryMessage.TestCase.SkipReason,
+                    }).ToList();
+                    if (details.Any())
+                    {
+                        var dictKey = details.First().TestName;
+                        if (Tests.ContainsKey(dictKey))
+                            Tests[dictKey].AddRange(details);
+                        else
+                            Tests.Add(dictKey, details);
+                    }
+                    break;
+                case IDiscoveryCompleteMessage discoveryComplete:
+                    Finished.Set();
+                    break;
+                case ErrorMessage err:
+                    Errors.Add(err);
+                    break;
+            }
+
+            return true;
+        }
+
+        private IEnumerable<RoleName> RoleNames(Type specType)
+        {
+            var ctorWithConfig = FindConfigConstructor(specType);
+            var configType = ctorWithConfig.GetParameters().First().ParameterType;
+            var args = ConfigConstructorParamValues(configType);
+            var configInstance = Activator.CreateInstance(configType, args);
+            var roleType = typeof(RoleName);
+            var configProps = configType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            var roleProps = configProps.Where(p => p.PropertyType == roleType && p.Name != "Myself").Select(p => (RoleName)p.GetValue(configInstance));
+            var configFields = configType.GetFields(BindingFlags.Instance | BindingFlags.Public);
+            var roleFields = configFields.Where(f => f.FieldType == roleType && f.Name != "Myself").Select(f => (RoleName)f.GetValue(configInstance));
+            var roles = roleProps.Concat(roleFields).Distinct();
+            return roles;
+        }
+
+        internal static ConstructorInfo FindConfigConstructor(Type configUser)
+        {
+            var baseConfigType = typeof(MultiNodeConfig);
+            var current = configUser;
+            while (current != null)
+            {
+
+#if CORECLR
+                var ctorWithConfig = current
+                    .GetConstructors(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .FirstOrDefault(c => null != c.GetParameters().FirstOrDefault(p => p.ParameterType.GetTypeInfo().IsSubclassOf(baseConfigType)));
+            
+                current = current.GetTypeInfo().BaseType;
+#else
+                var ctorWithConfig = current
+                    .GetConstructors(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .FirstOrDefault(c => null != c.GetParameters().FirstOrDefault(p => p.ParameterType.IsSubclassOf(baseConfigType)));
+
+                current = current.BaseType;
+#endif
+                if (ctorWithConfig != null) return ctorWithConfig;
+            }
+
+            throw new ArgumentException($"[{configUser}] or one of its base classes must specify constructor, which first parameter is a subclass of {baseConfigType}");
+        }
+
+        private object[] ConfigConstructorParamValues(Type configType)
+        {
+            var ctors = configType.GetConstructors(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            var empty = ctors.FirstOrDefault(c => !c.GetParameters().Any());
+
+#if CORECLR
+            return empty != null
+                ? new object[0]
+                : ctors.First().GetParameters().Select(p => p.ParameterType.GetTypeInfo().IsValueType ? Activator.CreateInstance(p.ParameterType) : null).ToArray();
+#else
+            return empty != null
+                ? new object[0]
+                : ctors.First().GetParameters().Select(p => p.ParameterType.IsValueType ? Activator.CreateInstance(p.ParameterType) : null).ToArray();
+#endif
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Finished.Dispose();
+        }
+    }
+}
---
+++ test/Akkatecture.NodeTestRunner/Sink.cs
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------------
+// <copyright file="Sink.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2018 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Akka.Actor;
+using Akka.MultiNodeTestRunner.Shared.Sinks;
+using Xunit;
+using Xunit.Abstractions;
+using IMessageSink = Xunit.Abstractions.IMessageSink;
+
+namespace Akka.NodeTestRunner
+{
+#if CORECLR
+    class Sink : IMessageSink, IDisposable
+#else
+    class Sink : MarshalByRefObject, IMessageSink, IDisposable
+#endif
+    {
+        public bool Passed { get; private set; }
+        public ManualResetEvent Finished { get; private set; }
+        private readonly int _nodeIndex;
+        private readonly string _nodeRole;
+
+        private readonly IActorRef _logger;
+
+        public Sink(int nodeIndex, string nodeRole, IActorRef logger) 
+        {
+            _nodeIndex = nodeIndex;
+            _nodeRole = nodeRole;
+            Finished = new ManualResetEvent(false);
+            _logger = logger;
+        }
+
+        public bool OnMessage(IMessageSinkMessage message)
+        {
+            var resultMessage = message as ITestResultMessage;
+            if (resultMessage != null)
+            {
+                _logger.Tell(resultMessage.Output);
+                Console.WriteLine(resultMessage.Output);
+            }
+            var testPassed = message as ITestPassed;
+            if (testPassed != null)
+            {
+                //the MultiNodeTestRunner uses 1-based indexing, which is why we have to add 1 to the index.
+                var specPass = new SpecPass(_nodeIndex + 1, _nodeRole, testPassed.TestCase.DisplayName);
+                _logger.Tell(specPass.ToString());
+                Console.WriteLine(specPass.ToString()); //so the message also shows up in the individual per-node build log
+                Passed = true;
+                return true;
+            }
+            var testFailed = message as ITestFailed;
+            if (testFailed != null)
+            {
+                //the MultiNodeTestRunner uses 1-based indexing, which is why we have to add 1 to the index.
+                var specFail = new SpecFail(_nodeIndex + 1, _nodeRole, testFailed.TestCase.DisplayName);
+                foreach (var failedMessage in testFailed.Messages) specFail.FailureMessages.Add(failedMessage);
+                foreach (var stackTrace in testFailed.StackTraces) specFail.FailureStackTraces.Add(stackTrace);
+                foreach(var exceptionType in testFailed.ExceptionTypes) specFail.FailureExceptionTypes.Add(exceptionType);
+                _logger.Tell(specFail.ToString());
+                Console.WriteLine(specFail.ToString());
+                return true;
+            }
+            var errorMessage = message as ErrorMessage;
+            if (errorMessage != null)
+            {
+                var specFail = new SpecFail(_nodeIndex + 1, _nodeRole, "ERRORED");
+                foreach (var failedMessage in errorMessage.Messages) specFail.FailureMessages.Add(failedMessage);
+                foreach (var stackTrace in errorMessage.StackTraces) specFail.FailureStackTraces.Add(stackTrace);
+                foreach (var exceptionType in errorMessage.ExceptionTypes) specFail.FailureExceptionTypes.Add(exceptionType);
+                _logger.Tell(specFail.ToString());
+                Console.WriteLine(specFail.ToString());
+            }
+            if (message is ITestAssemblyFinished)
+            {
+                Finished.Set();
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Finished.Dispose();
+        }
+    }
+}
+
