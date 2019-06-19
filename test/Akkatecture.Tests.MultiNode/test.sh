054fda2e829d8187edd582115b20b4e6004d817f
Lutando Ngqakaza <lutando@ngqakaza.com>
2019-06-19 15:03:46 +0200

    changes to the test script

--- test/Akkatecture.Tests.MultiNode/test.sh
+++ test/Akkatecture.Tests.MultiNode/test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#Clean
 rm -rf ./Akkatecture.Tests.MultiNode.AggregateClusterTests/
 rm -rf ./Akkatecture.Tests.MultiNode.AggregateSagaClusterTests/
 rm -rf ./bin/
@@ -8,7 +9,5 @@ rm -rf ./obj/
 dotnet build Akkatecture.Tests.MultiNode.csproj --configuration Release
 dotnet build ../Akkatecture.NodeTestRunner/Akkatecture.NodeTestRunner.csproj --configuration Release --runtime osx-x64
 dotnet build ../Akkatecture.MultiNodeTestRunner/Akkatecture.MultiNodeTestRunner.csproj --configuration Release --runtime osx-x64
+/bin/cp -rf ./bin/Release/netcoreapp2.2/Newtonsoft.Json.dll ../Akkatecture.MultiNodeTestRunner/bin/Release/netcoreapp2.2/osx-x64/Newtonsoft.Json.dll
 dotnet ../Akkatecture.MultiNodeTestRunner/bin/Release/netcoreapp2.2/osx-x64/Akka.MultiNodeTestRunner.dll ./bin/Release/netcoreapp2.2/Akkatecture.Tests.MultiNode.dll -Dmultinode.platform=netcore
-#/bin/cp -rf /bin/Release/netcoreapp2.2/Newtonsoft.Json.dll /tools/osx-x64/Akka.MultiNodeTestRunner
-
-#dotnet ./tools/osx-x64/Akka.MultiNodeTestRunner.dll ./bin/Release/netcoreapp2.2/Akkatecture.Tests.MultiNode.dll -Dmultinode.platform=netcore
