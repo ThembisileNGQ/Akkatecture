9d5b319ff4be984beae82c6346c3d2b5e7536965
Lutando Ngqakaza <lutando@ngqakaza.com>
2019-07-08 21:16:39 +0200

    add the current work in progress

--- build/README.md
+++ build/README.md
@@ -9,3 +9,14 @@ Akkatecture is built in azure pipelines. A description of each of the build pipe
 **azure-pipelines-release-ci-cd.yaml** - Runs the pipeline for production builds that go to NuGet. Packages are public and 'production ready'.
 
 **azure-pipelines-static-ci** - Runs a smaller build pipeline used for gating pull requests into both master and dev. no artefacts come from this build.
+
+# FAKE Build Scripts
+
+Right now it is still under development.
+
+The point of these build scripts is to enable cross plat testing scenarios. Run the following to get the Fake builder to work:
+
+`fake build`
+`fake run build.fsx --target Build` 
+
+This assumes that you have fake installed, and that you have followeded [these](http://fake.build/fake-commandline.html) steps. If not then run the shell or command scripts
--- build/build.fsx
+++ build/build.fsx
@@ -8,52 +8,154 @@ open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
-let configuration = "Release"
+
+Target.initEnvironment()
+
+// --------------------------------------------------------------------------------------
+// Build types
+// --------------------------------------------------------------------------------------
+
+type Platform =
+    | Windows
+    | Linux
+    | OSX
+
+type Host =
+    | Local
+    | AzureDevOps
+
+type Branch =
+    | Dev
+    | Master
+
+type FeedVersion =
+    | Alpha
+    | PreRelease
+    | NuGet
+
+// --------------------------------------------------------------------------------------
+// Build variables
+// --------------------------------------------------------------------------------------
+
+let host = match Environment.hasEnvironVar("BUILD_BUILDID") with
+            | true -> AzureDevOps
+            | false -> Local
+
+//let feedVersion = match Environment.hasEnvironVar("BUILD_BUILDID") with
+
+let platform =
+    if Environment.isMacOS then OSX
+    elif Environment.isLinux then Linux
+    else Windows
+
+let buildNumber = match host with
+                    | Local -> "0.0.1"
+                    | AzureDevOps -> Environment.environVarOrDefault "BUILD_BUILDNUMBER" "0.0.1"
+
+let runtimeIds = dict[Windows, "win-x64"; Linux, "linux-x64"; OSX, "osx-x64"]
+let runtimeId = runtimeIds.Item(platform);
+let configuration = DotNet.BuildConfiguration.Release
 let solution = System.IO.Path.GetFullPath(string "../Akkatecture.sln")
-System.Environment.CurrentDirectory <- System.IO.Path.GetFullPath(string "../")
+let sourceDirectory =  System.IO.Path.GetFullPath(string "../")
+let testResults = sourceDirectory @@ "testresults"
+
+// --------------------------------------------------------------------------------------
+// Build Current Working Directory
+// --------------------------------------------------------------------------------------
+System.Environment.CurrentDirectory <- sourceDirectory
+
+// --------------------------------------------------------------------------------------
+// Build Targets
+// --------------------------------------------------------------------------------------
 
 Target.create "Clean" (fun _ ->
-    Trace.log " --- Cleaning stuff --- "
+    Trace.log " --- Cleaning Solution --- "
+
     let cleanables = 
-        !! "../src/**/bin"
-        ++ "../src/**/obj"
+        !! "src/**/bin"
+        ++ "src/**/obj"
+        ++ "test/**/bin"
+        ++ "test/**/obj"
+        ++ testResults
+        
+    cleanables |> Shell.cleanDirs   
 
-    cleanables |> Shell.cleanDirs 
+    Trace.log " --- Build Variables ---"
+    Trace.logfn "Platform: %A" platform
+    Trace.logfn "RuntimeId: %s" runtimeId
+    Trace.logfn "Host: %A" host
+    Trace.logfn "BuildNumber: %s" buildNumber
 
-    cleanables |> Seq.iter Trace.trace
-    
-    Trace.trace System.Environment.CurrentDirectory
-    Trace.trace solution
+    //cleanables |> Seq.iter Trace.log 
+
 )
 
 Target.create "Restore" (fun _ ->
-    Trace.log " --- Restoring stuff --- "
+    Trace.log " --- Restoring Solution --- "
+
+    DotNet.restore id solution
 )
 
 Target.create "Build" (fun _ ->
-    Trace.log " --- Building stuff --- "
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
+    Trace.log " --- Building Projects --- "
+    let projects = 
+        !! "src/**/*.*proj"
+        ++ "test/Akkatecture.Tests/Akkatecture.Tests.csproj"
+
+    let buildOptions (defaults:DotNet.BuildOptions) =
+        { defaults with
+            MSBuildParams = 
+                { defaults.MSBuildParams with
+                    Properties = ["Version", buildNumber] }
+            NoRestore = true
+            Configuration = configuration }
+
+    projects |> Seq.iter (DotNet.build buildOptions)
+    
 )
 
 Target.create "Test" (fun _ ->
-    Trace.log " --- Testing stuff --- "
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
+    Trace.log " --- Unit Tests --- "
+    let projects = !! "test/Akkatecture.Tests/Akkatecture.Tests.csproj"
+    let coverletOutput = testResults </> "opencover.xml"
+    let testOptions (defaults:DotNet.TestOptions) =
+        { defaults with
+            MSBuildParams = 
+                { defaults.MSBuildParams with
+                    Properties = ["CollectCoverage", "true"; "CoverletOutputFormat", "opencover"; "CoverletOutput", coverletOutput; "Exclude", @"[xunit*]*,[Akkatecture.TestHelpers]*,[Akkatecture.Tests*]*,[*TestRunner*]*"] }
+            Configuration = configuration
+            NoBuild = true}
+
+    projects |> Seq.iter (DotNet.test testOptions)
 )
 
 Target.create "MultiNodeTest" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
+    Trace.log " --- Multi Node Tests --- "
+)
+
+Target.create "Push" (fun _ ->
+    Trace.log " --- Publish Packages --- "
+
+    let glob = sprintf "src/**/bin/%A/*.nupkg" configuration
+
+    let pushables =
+        !! glob
+
+    pushables |> Seq.iter Trace.log 
 )
 
 Target.create "All" ignore
+
+// --------------------------------------------------------------------------------------
+// Build order
+// --------------------------------------------------------------------------------------
 
 "Clean"
   ==> "Restore"
   ==> "Build"
   ==> "Test"
   ==> "MultiNodeTest"
+  ==> "Push"
   ==> "All"
 
-Target.runOrDefault "All"
+Target.runOrDefault "Build"
